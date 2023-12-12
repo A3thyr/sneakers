07fea26163d7fab87587a31ac5e14b5239a28414
A3thyr <ennjaletgo@yandex.ru>
2023-12-13 00:10:04 +0300

    feat: added Cart component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import Card from "./components/Card";
 import Header from "./components/Header";
+import Cart from "./components/Cart";
+
+const array = [
+  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price : 12999},
+  {name: 'Мужские Кроссовки Nike Air Max 270', price: 12999},
+];
 
 function App() {
   return (
     <div className="page">
-      <div style={{display: 'none'}} className="overlay">
-        <div className="overlay-modalcart">
-          <div className="overlay-modalcart-items">
-            <h2 className="overlay-modalcart-items-title">Корзина</h2>
-            <div className="overlay-modalcart-items-item">
-              <img className="overlay-modalcart-items-item-img" src="/img/sneakers3.jpg" alt="sneakers3" width={70} height={70}></img>
-              <div className="overlay-modalcart-items-item-text">
-                <p className="overlay-modalcart-items-item-text-p">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                <b className="overlay-modalcart-items-item-text-b">8 499 руб.</b>
-              </div>
-              <img src="/img/deletecart.svg" alt="deletebtn"></img>
-            </div>
-            <div className="overlay-modalcart-items-item">
-              <img className="overlay-modalcart-items-item-img" src="/img/sneakers3.jpg" alt="sneakers3" width={70} height={70}></img>
-              <div className="overlay-modalcart-items-item-text">
-                <p className="overlay-modalcart-items-item-text-p">Мужские Кроссовки Nike Blazer Mid Suede</p>
-                <b className="overlay-modalcart-items-item-text-b">8 499 руб.</b>
-              </div>
-              <img src="/img/deletecart.svg" alt="deletebtn"></img>
-            </div>
-          </div>
-          <div className="overlay-modalcart-btm">
-            <ul className="overlay-modalcart-btm-ul">
-              <li className="overlay-modalcart-btm-ul-li">
-                <span className="overlay-modalcart-btm-ul-li-span">Итого: </span>
-                <div className="overlay-modalcart-btm-ul-li-line"></div>
-                <b className="overlay-modalcart-btm-ul-li-b">21 498 руб. </b>
-              </li>
-              <li className="overlay-modalcart-btm-ul-li">
-                <span className="overlay-modalcart-btm-ul-li-span">Налог 5%: </span>
-                <div className="overlay-modalcart-btm-ul-li-line"></div>
-                <b className="overlay-modalcart-btm-ul-li-b">1074 руб.</b>
-              </li>
-            </ul>
-            <button className="overlay-modalcart-btm-buybtn">Оформить заказ
-              <img src="/img/arrow-r.svg" alt="arrowright"></img>
-            </button>
-          </div>
-        </div>
-      </div>
+      <Cart/>
       <Header></Header>
       <div className="content">
         <div className="content-top">
@@ -54,10 +21,11 @@ function App() {
           </div>
         </div>
         <div className="content-list">
+          {
+
+          }
           <Card />
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+          <Card />
         </div>
       </div>
     </div>
